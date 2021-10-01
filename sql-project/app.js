e5c6223cb15caaf81923411b086e4e84bcaca770
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-10-01 15:20:04 +0300

    classwork

--- sql-project/app.js
+++ sql-project/app.js
@@ -1,23 +1,55 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 const hbs = require('express-handlebars');
+const path = require('path');
+const db = require('./db/connection');
 
-//Generating connection to MySQL
-const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'myblog'
+app.use(express.urlencoded({
+    extended:false
+}));
+
+app.engine('hbs',hbs({
+    extname:'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/template'
+}));
+
+app.set('views', path.join(__dirname,'/views/'));
+
+app.set('view engine', 'hbs');
+
+app.use('/static', express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('add-company');
 });
-//Starting database and callback
-db.connect(err=>{
-    if(err){
-        console.log('Connection failed');
-        return;
-    }
-    console.log('Connected successfully');
+
+app.get('/add-company',(req,res)=>{
+    res.render('add-company');
 });
+
+app.post('/add-company',(req,res)=>{
+    let companyName = req.body.name;
+    let companyAddress = req.body.address;
+    db.query(`SELECT * FROM companies WHERE name = '${companyName}'`,(err,resp)=>{
+        if(resp.length == 0){
+            db.query(`INSERT INTO companies (name, address) VALUES (
+                '${companyName}','${companyAddress}')`,err=>{
+                    if(err){
+                        console.log(err);
+                        return;
+                    }
+                    res.redirect('/?message=Successfully added record');
+                }); 
+        }else {
+            res.redirect('/?message=Record already exists');
+        }
+    });
+
+});
+
+app.listen('3000');
+
 //SQL query
 //CREATE DATABASE <name>
 // db.query('DROP TABLE IF EXISTS records')
@@ -60,12 +92,12 @@ db.connect(err=>{
 //     }
 //     console.log(res);
 // });
-db.query(`SELECT * FROM records`,(err,res)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log(res);
-});
+// db.query(`SELECT * FROM records`,(err,res)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(res);
+// });
 // db.query(`UPDATE records SET name ='edited2', text ='edited text2' WHERE id = 1`,(err,res)=>{
 //     if(err){
 //         console.log(err);
@@ -96,13 +128,13 @@ db.query(`SELECT * FROM records`,(err,res)=>{
 //     }
 //     // console.log(res);
 // });
-db.query(`DELETE FROM records WHERE id>3`,(err,res)=>{
-    if(err){
-        console.log(err);
-    }
-});
-db.query(`ALTER TABLE records AUTO_INCREMENT=1`,(err,res)=>{
-    if(err){
-        console.log(err);
-    }
-});
+// db.query(`DELETE FROM records WHERE id>3`,(err,res)=>{
+//     if(err){
+//         console.log(err);
+//     }
+// });
+// db.query(`ALTER TABLE records AUTO_INCREMENT=1`,(err,res)=>{
+//     if(err){
+//         console.log(err);
+//     }
+// });
